df94479fdae6a606ed95503067533af3bdce814a
David Golden <dagolden@cpan.org>
2011-01-27 16:40:56 -0500

    initial import of Dist-Zilla-Plugin-OSPrereqs

---
+++ lib/Dist/Zilla/Plugin/OSPrereqs.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+use utf8;
+
+package Dist::Zilla::Plugin::OSPrereqs;
+# ABSTRACT: No abstract given for Dist::Zilla::Plugin::OSPrereqs
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+__END__
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use Dist::Zilla::Plugin::OSPrereqs;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
